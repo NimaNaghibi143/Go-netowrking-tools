273b53925f1f33ab642851bc7cf57fb9aeba4b2b
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-03-15 23:47:52 +0330

    feat: implement Read/Write inrefaces for the TCP server

--- tcp-scanners-proxies/cmd/tcpProxy/main.go
+++ tcp-scanners-proxies/cmd/tcpProxy/main.go
@@ -1,1 +1,12 @@
-package tcpproxy
+package main
+
+// Building a TCP Proxy
+// First let's start with a echo server
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+type Writer interface {
+	Write(p []byte) (n int, err error)
+}
